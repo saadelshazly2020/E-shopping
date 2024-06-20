b0b020c5b83aed17591b76339e7b8e166426a7e5
saad <saadelshazly5570@gmail.com>
2024-06-20 18:47:28 +0300

    add create product endpoint with carter

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,7 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 //add services to DI container
 
+builder.Services.AddCarter();
+builder
+    .Services.AddMediatR(config =>
+    {
+        config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+    });
+
 var app = builder.Build();
 //add middlewares to proj pipeline
-
+app.MapCarter();
 app.Run();
