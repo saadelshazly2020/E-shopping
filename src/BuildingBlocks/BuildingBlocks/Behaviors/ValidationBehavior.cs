76e9da8c198ea9d7196d42627a3541a959819ecc
saad <saadelshazly5570@gmail.com>
2024-06-27 12:34:50 +0300

    add fluent validation to MediatR pipline

---
+++ src/BuildingBlocks/BuildingBlocks/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,24 @@
+﻿using BuildingBlocks.CQRS;
+using FluentValidation;
+using MediatR;
+using System.Windows.Input;
+
+namespace BuildingBlocks.Behaviors
+{
+    public class ValidationBehavior<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validators)
+        : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : ICommand<TResponse>
+    {
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            var context = new ValidationContext<TRequest>(request);
+            var validationResult=await Task.WhenAll(validators.Select(x=>x.ValidateAsync(context, cancellationToken)));
+            var failures = validationResult.Where(x => x.Errors.Any()).SelectMany(x => x.Errors).ToList();
+            if (failures.Any())
+            {
+                throw new ValidationException(failures);
+            }
+            return await next();
+        }
+    }
+}
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,12 +1,17 @@
+
+using BuildingBlocks.Behaviors;
+
 var builder = WebApplication.CreateBuilder(args);
 //add services to DI container
 
 builder.Services.AddCarter();
-builder
-    .Services.AddMediatR(config =>
+builder.Services.AddMediatR(config =>
     {
         config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+        config.AddOpenBehavior(typeof(ValidationBehavior<,>));
     });
+
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
 builder.Services.AddMarten(config =>
 {
     config.Connection(builder.Configuration.GetConnectionString("Database")!);
