760c52f946ebd89db81cee3c5c51aeabd2d37235
saad <saadelshazly5570@gmail.com>
2024-06-27 12:35:03 +0300

    create produt validation

--- src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -6,3 +6,4 @@ global using Marten;
 global using BuildingBlocks.CQRS;
 global using Catalog.API.Exceptions;
 global using Catalog.API.Models;
+global using FluentValidation;
--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductCommandHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductCommandHandler.cs
@@ -5,7 +5,17 @@ namespace Catalog.API.Products.CreateProduct
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
         : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-
+    public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommandValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+            RuleFor(x => x.Category).NotEmpty().WithMessage("Name is required");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Name is required");
+            RuleFor(x => x.ImageFile).NotEmpty().WithMessage("Name is required");
+            RuleFor(x => x.Price).GreaterThan(0).WithMessage("price must be greater than 0");
+        }
+    }
     public class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
