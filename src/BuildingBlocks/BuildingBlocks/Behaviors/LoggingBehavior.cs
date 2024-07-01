51f42dfc32c4da28f5392bcc0fbf619ea17d9de0
saad <saadelshazly5570@gmail.com>
2024-07-01 09:06:16 +0300

    issue with logging behavior

--- src/BuildingBlocks/BuildingBlocks/Behaviors/LoggingBehavior.cs
+++ src/BuildingBlocks/BuildingBlocks/Behaviors/LoggingBehavior.cs
@@ -11,7 +11,7 @@ namespace BuildingBlocks.Behaviors
 {
     public class LoggingBehavior<TRequest, TResponse>(ILogger<LoggingBehavior<TRequest, TResponse>> logger)
         : IPipelineBehavior<TRequest, TResponse>
-        where TRequest : notnull, IRequest<TRequest>
+        where TRequest : notnull, IRequest<TResponse>
         where TResponse : notnull
     {
         public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
