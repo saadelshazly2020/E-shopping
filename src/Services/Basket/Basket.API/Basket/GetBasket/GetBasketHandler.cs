20780a9ee5b714993ae1f63ec1612b268f22158d
saad <saadelshazly5570@gmail.com>
2024-07-07 16:33:16 +0300

    add basket use cases

---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Basket.API.Basket.GetBasket
+{
+
+    public record GetBasketQuery(string Username):IQuery<GetBasketResult>;
+    public record GetBasketResult(ShoppingCart Cart);
+    public class GetBasketQueryHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
+    {
+        public async Task<GetBasketResult> Handle(GetBasketQuery request, CancellationToken cancellationToken)
+        {
+            //to do get basket from repository
+
+            return new GetBasketResult(new ShoppingCart("swn"));
+        }
+    }
+}
