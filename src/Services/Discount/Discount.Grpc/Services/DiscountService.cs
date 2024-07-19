296421981bb5edf6aeb2ba8a8af8f836b93e1118
saad <saadelshazly5570@gmail.com>
2024-07-19 17:03:01 +0300

    add discount service that implement DiscountProtoService.DiscountProtoServiceBase

---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,28 @@
+﻿using Discount.Grpc.Protos;
+using Grpc.Core;
+
+namespace Discount.Grpc.Services
+{
+    public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+    {
+        public override Task<CopounModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            //get discount from database
+            return base.GetDiscount(request, context);
+        }
+        public override Task<CopounModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+        {
+            return base.CreateDiscount(request, context);
+        }
+
+        public override Task<CopounModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+        {
+            return base.UpdateDiscount(request, context);
+        }
+
+        public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+        {
+            return base.DeleteDiscount(request, context);
+        }
+    }
+}
