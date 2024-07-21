83343d374ef0141ade0af554d3b43e1dfbbbfc93
saad <saadelshazly5570@gmail.com>
2024-07-21 22:50:47 +0300

    add Grpc client stub to basket

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,9 +1,10 @@
 ﻿
 using Basket.API.Data;
+using Discount.Grpc.Protos;
 
 namespace Basket.API.Basket.StoreBasket
 {
-    public record StoreBasketCommand(ShoppingCart Cart):ICommand<StoreBasketResult>;
+    public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
     public record StoreBasketResult(string UserName);
 
     public class StorebasketCommandValidator : AbstractValidator<StoreBasketCommand>
@@ -14,13 +15,24 @@ namespace Basket.API.Basket.StoreBasket
             RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("UserName is required");
         }
     }
-    public class StoreBasketCommandHandler(IBasketRepository repository) : ICommandHandler<StoreBasketCommand, StoreBasketResult>
-    {   
+    public class StoreBasketCommandHandler(IBasketRepository repository, DiscountProtoService.DiscountProtoServiceClient protoClient) : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+    {
         public async Task<StoreBasketResult> Handle(StoreBasketCommand request, CancellationToken cancellationToken)
         {
+
+            await DeductDiscount(request.Cart, cancellationToken);
             //store cart to db
-           var basket=await  repository.StoreBasket(request.Cart,cancellationToken);
+            var basket = await repository.StoreBasket(request.Cart, cancellationToken);
             return new StoreBasketResult(basket.UserName);
+        }
+
+        private async Task DeductDiscount(ShoppingCart cart, CancellationToken cancellationToken)
+        {
+            foreach (var item in cart.Items)
+            {
+                var coupon = await protoClient.GetDiscountAsync(new GetDiscountRequest { ProductName = item.ProductName });
+                item.Price -= coupon.Amount;
+            }
         }
     }
 }
