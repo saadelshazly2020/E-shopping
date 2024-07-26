b3d88886089ed62722da7a309f71557b3ec2ace3
saad <saadelshazly5570@gmail.com>
2024-07-26 15:10:59 +0300

    DDD Abstractions

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/Aggregate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.Abstractions
+{
+    public abstract class Aggregate<TId> : Entity<TId>, IAggregate<TId>
+    {
+        private readonly List<IDomainEvent> _domainEvents = new();
+
+        public IReadOnlyList<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+        public void AddDomainEvent(IDomainEvent domainEvent)
+        {
+            _domainEvents.Add(domainEvent);
+        }
+
+        public IDomainEvent[] ClearDomainEvents()
+        {
+            IDomainEvent[] deQueuedDomainEvents=_domainEvents.ToArray();
+
+            _domainEvents.Clear();
+
+            return deQueuedDomainEvents;
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.Abstractions
+{
+    public interface IEntity<T> : IEntity
+    {
+        public T Id { get; set; }
+    }
+    public interface IEntity
+    {
+        public string? CreatedBy { get; set; }
+        public DateTime? CreateAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public string? UpdatedBy { get; set; }
+    }
+}
