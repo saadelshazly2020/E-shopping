7befa7f5e8229cdf6fb8bf8d4d2b208878aa20c3
saad <saadelshazly5570@gmail.com>
2024-07-28 22:58:02 +0300

    add sql image to docker compose and apply migrations to database

--- src/Services/Ordering/Ordering.Domain/Models/Product.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Product.cs
@@ -1,8 +1,11 @@
-﻿namespace Ordering.Domain.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Ordering.Domain.Models
 {
     public class Product : Entity<ProductId>
     {
         public string Name { get; set; } = default!;
+
         public decimal Price { get; set; } = default!;
     }
 }
