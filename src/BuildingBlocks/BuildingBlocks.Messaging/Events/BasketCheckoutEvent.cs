be418a57c93f4945e23b41ab0a39b880fd23792e
saad <saadelshazly5570@gmail.com>
2024-08-18 14:29:08 +0300

    add buildingBlock.Message Class library with events and massTransit extension

---
+++ src/BuildingBlocks/BuildingBlocks.Messaging/Events/BasketCheckoutEvent.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingBlocks.Messaging.Events
+{
+    public record BasketCheckoutEvent : IntegrationEvent
+    {
+        public string UserName { get; set; } = default!;
+        public Guid CustomerId { get; set; } = default!;
+        public decimal TotalPrice { get; set; } = default!;
+
+        //shepping and billing address
+        public string FirstName { get; private set; } = default!;
+        public string LastName { get; private set; } = default!;
+        public string EmailAddress { get; private set; } = default!;
+        public string AddressLine { get; private set; } = default!;
+        public string Country { get; private set; } = default!;
+        public string State { get; private set; } = default!;
+        public string ZipCode { get; private set; } = default!;
+
+        //Payment 
+        public string CardName { get; } = default!;
+        public string CardNumber { get; } = default!;
+        public string Expiration { get; } = default!;
+        public string CVV { get; } = default!;
+        public int PaymentMethod { get; } = default!;
+
+
+    }
+}
---
+++ src/BuildingBlocks/BuildingBlocks.Messaging/MassTransit/Extensions.cs
@@ -0,0 +1,36 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+namespace BuildingBlocks.Messaging.MassTransit
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddMessageBroker(this IServiceCollection services,IConfiguration configuration, Assembly? assembly = null)
+        {
+            //add message broker configuration
+            services.AddMassTransit(config => {
+
+                config.SetKebabCaseEndpointNameFormatter();
+
+                if (assembly != null)
+                    config.AddConsumers(assembly);
+
+                //add rabbitMQ config
+                config.UsingRabbitMq((context, configrator) =>
+                {
+                    configrator.Host(new Uri(configuration["MessageBroker:Host"]!), host =>
+                    {
+                        host.Username(configuration["MessageBroker:UserName"]!);
+                        host.Password(configuration["MessageBroker:Password"]!);
+                    });
+                    configrator.ConfigureEndpoints(context);
+
+                });
+
+                
+            });
+            return services;
+        }
+    }
+}
