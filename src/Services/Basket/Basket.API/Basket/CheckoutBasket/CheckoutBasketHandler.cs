b060385aaf9c7c61d49861d7606dcc7000b2819b
saad <saadelshazly5570@gmail.com>
2024-08-18 16:47:12 +0300

    basket checkout use case [enpoint and handler]

---
+++ src/Services/Basket/Basket.API/Basket/CheckoutBasket/CheckoutBasketHandler.cs
@@ -0,0 +1,39 @@
+﻿using Basket.API.Dtos;
+using BuildingBlocks.Messaging.Events;
+using MassTransit;
+
+namespace Basket.API.Basket.CheckoutBasket
+{
+    public record CheckoutBasketCommand(CheckoutBasketDto CheckoutBasketDto) : ICommand<CheckoutBasketResult>;
+    public record CheckoutBasketResult(bool IsSuccess);
+
+    public class CheckoutBasketValidator : AbstractValidator<CheckoutBasketCommand>
+    {
+        public CheckoutBasketValidator()
+        {
+            RuleFor(x => x.CheckoutBasketDto).NotNull().WithMessage("CheckoutBasketDto is required");
+            RuleFor(x => x.CheckoutBasketDto.UserName).NotNull().WithMessage("UserName is required");
+        }
+    }
+
+    public class CheckoutBasketHandler(IBasketRepository repository, IPublishEndpoint publishEndpoint) : ICommandHandler<CheckoutBasketCommand, CheckoutBasketResult>
+    {
+        public async Task<CheckoutBasketResult> Handle(CheckoutBasketCommand command, CancellationToken cancellationToken)
+        {
+            //get basket from db
+            var basket = await repository.GetBasket(command.CheckoutBasketDto.UserName, cancellationToken);
+            if (basket == null)
+            {
+                return new CheckoutBasketResult(false);
+            }
+            //map and set total price to basket checkout event message
+            var eventMessage = command.CheckoutBasketDto.Adapt<BasketCheckoutEvent>();
+            eventMessage.TotalPrice = basket.TotalPrice;
+            //publish the basket check event to rabbitMQ 
+            await publishEndpoint.Publish(eventMessage, cancellationToken);
+            //delete basket
+            await repository.DeleteBasket(command.CheckoutBasketDto.UserName, cancellationToken);
+            return new CheckoutBasketResult(true);
+        }
+    }
+}
---
+++ src/Services/Basket/Basket.API/Dtos/CheckoutBasketDto.cs
@@ -0,0 +1,25 @@
+﻿namespace Basket.API.Dtos
+{
+    public class CheckoutBasketDto
+    {
+        public string UserName { get; set; } = default!;
+        public Guid CustomerId { get; set; } = default!;
+        public decimal TotalPrice { get; set; } = default!;
+
+        //shepping and billing address
+        public string FirstName { get; private set; } = default!;
+        public string LastName { get; private set; } = default!;
+        public string EmailAddress { get; private set; } = default!;
+        public string AddressLine { get; private set; } = default!;
+        public string Country { get; private set; } = default!;
+        public string State { get; private set; } = default!;
+        public string ZipCode { get; private set; } = default!;
+
+        //Payment 
+        public string CardName { get; } = default!;
+        public string CardNumber { get; } = default!;
+        public string Expiration { get; } = default!;
+        public string CVV { get; } = default!;
+        public int PaymentMethod { get; } = default!;
+    }
+}
