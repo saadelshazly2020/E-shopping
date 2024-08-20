0ff68f8bcd86e27c2b6319ce4066f9a7fa06ba0d
saad <saadelshazly5570@gmail.com>
2024-08-20 13:44:30 +0300

    solve issue with set accessor

--- src/Services/Basket/Basket.API/Dtos/CheckoutBasketDto.cs
+++ src/Services/Basket/Basket.API/Dtos/CheckoutBasketDto.cs
@@ -7,19 +7,19 @@
         public decimal TotalPrice { get; set; } = default!;
 
         //shepping and billing address
-        public string FirstName { get; private set; } = default!;
-        public string LastName { get; private set; } = default!;
-        public string EmailAddress { get; private set; } = default!;
-        public string AddressLine { get; private set; } = default!;
-        public string Country { get; private set; } = default!;
-        public string State { get; private set; } = default!;
-        public string ZipCode { get; private set; } = default!;
+        public string FirstName { get;  set; } = default!;
+        public string LastName { get;  set; } = default!;
+        public string EmailAddress { get;  set; } = default!;
+        public string AddressLine { get;  set; } = default!;
+        public string Country { get;  set; } = default!;
+        public string State { get;  set; } = default!;
+        public string ZipCode { get;  set; } = default!;
 
         //Payment 
-        public string CardName { get; } = default!;
-        public string CardNumber { get; } = default!;
-        public string Expiration { get; } = default!;
-        public string CVV { get; } = default!;
-        public int PaymentMethod { get; } = default!;
+        public string CardName { get; set; } = default!;
+        public string CardNumber { get; set; } = default!;
+        public string Expiration { get; set; } = default!;
+        public string CVV { get; set; } = default!;
+        public int PaymentMethod { get; set; } = default!;
     }
 }
