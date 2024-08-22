cc9de53918a188ef23b65a1d27e9ec9c5cc37d68
saad <saadelshazly5570@gmail.com>
2024-08-22 11:43:44 +0300

    apply api gateway settings and rate limitting

--- src/ApiGateways/YarpApiGateway/Program.cs
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,9 +1,25 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //add services to DI
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddFixedWindowLimiter("fixed", opt =>
+    {
+        opt.PermitLimit = 4;
+        opt.Window = TimeSpan.FromSeconds(12);
+
+    });
+});
 
 var app = builder.Build();
 
 //add http request pipelines
+app.UseRateLimiter();
+app.MapReverseProxy();
+
 
 app.Run();
